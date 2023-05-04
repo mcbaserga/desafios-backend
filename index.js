5e2953a3fdf5b8ebc555f1ce7e8168b0874c73a6
Maria Clara Baserga <mcbaserga@gmail.com>
2023-05-04 09:31:05 -0300

    primer desafio

---
+++ index.js
@@ -0,0 +1,48 @@
+class ProductManager {
+    #products
+    #error
+    constructor(){
+        this.#products = []
+        this.#error = undefined
+    }
+    
+    getProducts = () => this.#products
+
+    getProductById = (id) => {
+        const product = this.#products.find(item=> item.id === id)
+        if (!product) return "Not found"
+        return product
+    }
+    
+    #generateId = () => (this.#products.length === 0) ? 1 : this.#products[this.#products.length-1].id + 1
+
+    #validate = (title, description, price, thumbnail, code, stock) => {
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            this.#error = `${title} tiene campos incompletos` 
+        } else {
+            const found = this.#products.find(item => item.code === code)
+            if (found) this.#error = `${title} el código ya existe` 
+            else this.#error = undefined
+        }      
+    } 
+    
+
+    addProduct = (title, description, price, thumbnail, code, stock) => {
+        this.#validate(title, description, price, thumbnail, code, stock)
+        if (this.#error === undefined) 
+            this.#products.push({id: this.#generateId(), title, description, price, thumbnail, code, stock})
+        else 
+            console.log(this.#error)
+    }
+}
+
+const productManager = new ProductManager()
+productManager.addProduct('Adidas Adizero', 'Zapatillas deportivas', 30000, 'https://craftypixels.com/placeholder-image/300', 1010, 4)
+productManager.addProduct('Adidas Runfalcon', 'Zapatillas deportivas', 40000, 'https://craftypixels.com/placeholder-image/300', 1020, 3)
+productManager.addProduct('Adidas Predator', 'Zapatillas deportivas', 20000, 'https://craftypixels.com/placeholder-image/300', 1030, 6)
+productManager.addProduct('Adidas Forum', 'Zapatillas deportivas', 50000, 'https://craftypixels.com/placeholder-image/300', 1030, 1)
+productManager.addProduct('Adidas Galaxy', 'Zapatillas deportivas', 60000, 1050, 10)
+
+
+console.log(productManager.getProducts())
+console.log(productManager.getProductById(7))
